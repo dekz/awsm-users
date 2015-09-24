a37386777b80600b30326a117af6ff2c4894edfc
Jacob Evans <jacob@dekz.net>
2015-09-24 19:29:50 +1000

    Added list lambda

---
+++ list/index.js
@@ -0,0 +1,30 @@
+/**
+ * AWS Module: Action: Modularized Code
+ */
+
+var Promise = require("bluebird");
+var AWS = require('aws-sdk');
+var crypto = require('crypto');
+var util = require('util');
+
+var dynamodb = new AWS.DynamoDB();
+Promise.promisifyAll(Object.getPrototypeOf(dynamodb));
+
+// Export For Lambda Handler
+module.exports.run = function(event, context, cb) {
+  action(event).then(function(result) {
+    console.log('Done');
+    cb(null, result);
+  }).catch(function(error) {
+    console.log('Failed');
+    cb(error, null);
+  });
+};
+
+var listUsers = function(user, cb) {
+  return dynamodb.scanAsync({ TableName: 'jaws-users' });
+}
+
+var action = function(event) {
+  return listUsers(event);
+};
