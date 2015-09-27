60341953bc18848be43ff3784f642f8ef3da7f60
Jacob Evans <jacob@dekz.net>
2015-09-27 10:21:25 +1000

    Begin authentication

--- authenticate/index.js
+++ authenticate/index.js
@@ -1,29 +1,62 @@
-/**
- * AWS Module: Action: Modularized Code
- */
+var Promise  = require('bluebird'),
+    AWS      = require('aws-sdk'),
+    bcryptjs = require('bcryptjs'),
+    jwt      = require('jsonwebtoken'),
+    debug    = require('debug')('awsm-users');
 
-// Export For Lambda Handler
+var dynamodb = new AWS.DynamoDB();
+Promise.promisifyAll(Object.getPrototypeOf(dynamodb));
+
 module.exports.run = function(event, context, cb) {
-  return cb(null, action(event));
+  return action(event)
+    .then(function(result) {
+      debug('Success: Authentication');
+      cb(null, { status: 201, jwt: result });
+    })
+    .error(function(error) {
+      debug('Failed: %s', JSON.stringify(error));
+      cb(null, { status: 400, message: 'Authentication Failed' });
+    });
 };
 
-var AWS = require('aws-sdk');
-var crypto = require('crypto');
-var util = require('util');
-var dynamodb = new AWS.DynamoDB();
+var getUser = function(data) {
+  return [data, dynamodb.getItemAsync({
+    TableName: 'jaws-users',
+    Key: {
+      email: { S: data.email }
+    }
+  })];
+}
 
-var storeUser = function(user) {
-  dynamodb.putItem({
-    TableName: 'users',
-    Item: {
-      email:    { S: user.email },
-      password: { S: user.password },
-    },
-    ConditionExpression: 'attribute_not_exists (email)'
+var authenticate = function(data, user) {
+  var user = data[1],
+      data = data[0];
+  return new Promise(function(resolve, reject) {
+    if (bcryptjs.compareSync(data.password, user.password)) {
+      debug('successful auth compare');
+      resolve(user);
+    } else {
+      debug('failed auth compare');
+      reject(data);
+    }
+  });
+}
+
+var createToken = function(user) {
+  return new Promise(function(resolve, reject) {
+    var token = jwt.sign({
+      uid: user.id,
+    }, process.env.JWT_SECRET, {
+      issuer: process.env.JWT_ISSUER,
+      expiresInSeconds: 10
+    });
+
+    resolve(token);
   });
 }
 
 var action = function(event) {
-  storeUser(event);
-  return {message: 'You\'re JAWS lambda executed successfully!'};
+  return getUser(event)
+    .then(authenticate)
+    .then(createToken);
 };
--- list/index.js
+++ list/index.js
@@ -12,13 +12,15 @@ Promise.promisifyAll(Object.getPrototypeOf(dynamodb));
 
 // Export For Lambda Handler
 module.exports.run = function(event, context, cb) {
-  action(event).then(function(result) {
-    console.log('Done');
-    cb(null, result);
-  }).catch(function(error) {
-    console.log('Failed');
-    cb(error, null);
-  });
+  action(event)
+    .then(function(result) {
+      console.log('Done');
+      cb(null, result);
+    })
+    .error(function(error) {
+      console.log('Failed');
+      cb(error, null);
+    });
 };
 
 var listUsers = function(user, cb) {
